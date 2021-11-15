4e40369fa1662860688870d3e79cb9030caf9a95
Zreal <zenglz_pro@163.com>
2021-11-15 14:00:52 +0800

    change directory structure

--- bitcask/config.go
+++
@@ -1,9 +0,0 @@
-package bitcask
-
-const defaultMetaName string = "meta.json"
-const defaultFileDirName string = "data"
-const defaultIndexName string = "hint"
-const DELETEFLAG string = "#####"
-const defaultIndexValid bool = true
-const MaxDuplicatedIten uint32 = 50
-const MaxAppendFileSize int64 = 1024 * 1024 * 4
